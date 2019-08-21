434b2bda0381cfaa85cb5ca07d06d12def7cfb36
OOjeser <OOjeser@github.com>
2019-08-21 16:40:07 +0500

    Start

---
+++ calk/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace calk
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public List<Point> points = new List<Point>();
+        public List<decimal> lens = new List<decimal>();
+        public bool end;
+        public Brush PolyC = new SolidColorBrush(Color.FromRgb(200, 155, 255));
+        public Brush DotC = Brushes.Blue;
+        public Brush LineC = Brushes.Black;
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!end)
+            {
+                if (e.RightButton == MouseButtonState.Pressed)
+                {
+                    lens.Add(decimal.Parse(l.Text));
+                    rez.Text = calk().ToString();
+                    end = true;
+                }
+                else if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    Point p = e.GetPosition(this);
+                    if (points.Count >= 1)
+                        lens.Add(decimal.Parse(l.Text));
+                    points.Add(new Point(p.X - 10, p.Y - 30));
+                }
+                draw();
+            }
+            else
+            {
+                points = new List<Point>();
+                lens = new List<decimal>();
+                gr.Children.Clear();
+                end = false;
+            }
+        }
+
+        private decimal calk()
+        {
+            decimal height = decimal.Parse(h.Text);
+            decimal rezult = 0;
+            foreach (decimal d in lens)
+            {
+                rezult += d * height;
+            }
+            foreach (Grid g in otv.Items)
+            {
+                rezult -= decimal.Parse(((TextBox)g.Children[0]).Text) * decimal.Parse(((TextBox)g.Children[1]).Text);
+            }
+            return rezult;
+        }
+
+        private void calk(object sender, RoutedEventArgs e)
+        {
+            rez.Text = calk().ToString();
+        }
+
+        private void draw()
+        {
+            gr.Children.Clear();
+            gr.Children.Add(new Polygon() { Points = new PointCollection(points), Fill = PolyC });
+            foreach (Point p in points)
+            {
+                gr.Children.Add(
+                    new Ellipse()
+                    {
+                        Height = 4,
+                        Width = 4,
+                        Margin = new Thickness(p.X - 2, p.Y - 2, 0, 0),
+                        Fill = DotC,
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top
+                    }
+                    );
+                if (!end)
+                {
+                    gr.Children.Add(new Line() { X1 = 0, Y1 = p.Y, X2 = 10000, Y2 = p.Y, Stroke = LineC, StrokeThickness = 1 });
+                    gr.Children.Add(new Line() { X1 = p.X, Y1 = 0, X2 = p.X, Y2 = 10000, Stroke = LineC, StrokeThickness = 1 });
+                }
+            }
+        }
+
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void PolyColor(object sender, RoutedEventArgs e)
+        {
+            PolyC = (sender as MenuItem).Background;
+        }
+
+        private void DotColor(object sender, RoutedEventArgs e)
+        {
+            DotC = (sender as MenuItem).Background;
+        }
+
+        private void LineColor(object sender, RoutedEventArgs e)
+        {
+            LineC = (sender as MenuItem).Background;
+        }
+
+        private void Button_Plus(object sender, RoutedEventArgs e)
+        {
+            Grid g = new Grid { Background = Brushes.Black, Width = 105, Height = 35 };
+            g.Children.Add(new TextBox { HorizontalAlignment = HorizontalAlignment.Left, Width = 50, Height = 25, Text = "0" });
+            g.Children.Add(new TextBox { HorizontalAlignment = HorizontalAlignment.Right, Width = 50, Height = 25, Text = "0" });
+            otv.Items.Add(g);
+        }
+    }
+}
