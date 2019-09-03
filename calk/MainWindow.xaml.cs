099c004bc327c586c189b0d8c9749e9d3df0cdf3
OOjeser <OOjeser@github.com>
2019-09-03 17:23:37 +0500

    Some Update
    Update Setting
    Add lens

--- calk/MainWindow.xaml.cs
+++ calk/MainWindow.xaml.cs
@@ -15,14 +15,30 @@ namespace calk
         public MainWindow()
         {
             InitializeComponent();
+            updateParam();
         }
 
         public List<Point> points = new List<Point>();
         public List<decimal> lens = new List<decimal>();
         public bool end;
-        public Brush PolyC = new SolidColorBrush(Color.FromRgb(200, 155, 255));
-        public Brush DotC = Brushes.Blue;
-        public Brush LineC = Brushes.Black;
+        public Brush PolyC;
+        public Brush DotC;
+        public Brush LineC;
+        public Brush TextC;
+        public int TextS;
+        public double LineH;
+
+        public void updateParam()
+        {
+            PolyC = new SolidColorBrush(Properties.Settings.Default.PolyC);
+            DotC = new SolidColorBrush(Properties.Settings.Default.DotC);
+            LineC = new SolidColorBrush(Properties.Settings.Default.LineC);
+            TextC = new SolidColorBrush(Properties.Settings.Default.FontC);
+            TextS = Properties.Settings.Default.FontS;
+            LineH = Properties.Settings.Default.LineH;
+            draw();
+            Properties.Settings.Default.Save();
+        }
 
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -36,7 +52,7 @@ namespace calk
                         if (points.Count >= 1)
                         {
                             InputBox.InputBox inputBox = new InputBox.InputBox();
-                            lens.Add(decimal.Parse(inputBox.getString()));
+                            lens.Add(decimal.Parse(inputBox.getString().Replace(".", ",")));
                         }
                         points.Add(new Point(p.X - 10, p.Y - 30));
                     }
@@ -63,7 +79,7 @@ namespace calk
             catch
             {
                 InputBox.InputBox inputBox = new InputBox.InputBox("Введите высоту");
-                h.Text = inputBox.getString();
+                h.Text = inputBox.getString().Replace(".", ",");
                 height = decimal.Parse(h.Text);
             }
             decimal rezult = 0;
@@ -73,7 +89,7 @@ namespace calk
             }
             foreach (Grid g in otv.Items)
             {
-                rezult -= decimal.Parse(((TextBox)g.Children[0]).Text) * decimal.Parse(((TextBox)g.Children[1]).Text);
+                rezult -= decimal.Parse(((TextBox)g.Children[0]).Text.Replace(".",",")) * decimal.Parse(((TextBox)g.Children[1]).Text.Replace(".", ","));
             }
             return rezult;
         }
@@ -83,9 +99,10 @@ namespace calk
             if (points.Count != lens.Count)
             {
                 InputBox.InputBox inputBox = new InputBox.InputBox();
-                lens.Add(decimal.Parse(inputBox.getString()));
+                lens.Add(decimal.Parse(inputBox.getString().Replace(".", ",")));
             }
             rez.Text = calk().ToString();
+            draw();
         }
 
         private void draw()
@@ -107,12 +124,33 @@ namespace calk
                     );
                 if (!end)
                 {
-                    gr.Children.Add(new Line() { X1 = 0, Y1 = p.Y, X2 = 10000, Y2 = p.Y, Stroke = LineC, StrokeThickness = 1 });
-                    gr.Children.Add(new Line() { X1 = p.X, Y1 = 0, X2 = p.X, Y2 = 10000, Stroke = LineC, StrokeThickness = 1 });
+                    gr.Children.Add(new Line() { X1 = 0, Y1 = p.Y, X2 = 10000, Y2 = p.Y, Stroke = LineC, StrokeThickness = LineH });
+                    gr.Children.Add(new Line() { X1 = p.X, Y1 = 0, X2 = p.X, Y2 = 10000, Stroke = LineC, StrokeThickness = LineH });
                 }
             }
+            for (int i = 0; i < points.Count; i++)
+            {
+                try
+                {
+                    Point pos = FindTxtPos(points[i], points[i == points.Count - 1 ? 0 : i + 1]);
+                    gr.Children.Add(new TextBlock { Text = lens[i].ToString(), Margin = new Thickness(pos.X, pos.Y, 0, 0), FontSize = TextS, Foreground = TextC });
+                }catch{}
+            }
+            gr.Children.Add(new TextBlock { Text = Area(points.ToArray()).ToString() });
         }
 
+        double Area(Point[] polygon)
+        {
+            double area = 0;
+            for (int i = 0; i < polygon.Length; i++)
+            {
+                int j = (i + 1) % polygon.Length;
+                Point a = polygon[i], b = polygon[j];
+
+                area += a.X * b.Y - a.Y * b.X;
+            }
+            return System.Math.Abs(area) / 2;
+        }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
@@ -121,17 +159,25 @@ namespace calk
 
         private void PolyColor(object sender, RoutedEventArgs e)
         {
-            PolyC = (sender as MenuItem).Background;
+            Properties.Settings.Default.PolyC = ((SolidColorBrush)(sender as MenuItem).Background).Color;
+            updateParam();
         }
 
         private void DotColor(object sender, RoutedEventArgs e)
         {
-            DotC = (sender as MenuItem).Background;
+            Properties.Settings.Default.DotC = ((SolidColorBrush)(sender as MenuItem).Background).Color;
+            updateParam();
+        }
+        private void TxtColor(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.FontC = ((SolidColorBrush)(sender as MenuItem).Background).Color;
+            updateParam();
         }
 
         private void LineColor(object sender, RoutedEventArgs e)
         {
-            LineC = (sender as MenuItem).Background;
+            Properties.Settings.Default.LineC = ((SolidColorBrush)(sender as MenuItem).Background).Color;
+            updateParam();
         }
 
         private void Button_Plus(object sender, RoutedEventArgs e)
@@ -148,6 +194,19 @@ namespace calk
             lens.Clear();
             gr.Children.Clear();
             otv.Items.Clear();
+        }
+
+        public Point FindTxtPos(Point p1, Point p2) => new Point((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2);
+
+        private void FontS(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.FontS = int.Parse((sender as MenuItem).Header.ToString());
+            updateParam();
+        }
+        private void LineSize(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.LineH = double.Parse((sender as MenuItem).Header.ToString());
+            updateParam();
         }
     }
 }
